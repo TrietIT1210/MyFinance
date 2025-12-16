98cbb517f10b89cf017ba078f5ed8824c603d102
TrietIT1210 <trietminh1210@gmail.com>
2025-12-17 00:05:03 +0700

    Add expense details

--- app/src/main/java/com/example/myfinanceapp/Activities/DashboardActivity/screens/MainScreen.kt
+++ app/src/main/java/com/example/myfinanceapp/Activities/DashboardActivity/screens/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myfinanceapp.Activities.DashboardActivity.components.ActionButtonRow
 import com.example.myfinanceapp.Activities.DashboardActivity.components.CardSection
+import com.example.myfinanceapp.Activities.DashboardActivity.components.ExpenseItem
 import com.example.myfinanceapp.Activities.DashboardActivity.components.HeaderSection
 import com.example.myfinanceapp.Domain.ExpenseDomain
 
@@ -20,10 +22,10 @@ import com.example.myfinanceapp.Domain.ExpenseDomain
 @Preview(showBackground = true)
 fun MainScreenPreview() {
     val expenses = listOf(
-        ExpenseDomain("Restaurant", price = 573.12, pic = "img1", time = "17 June 2025 19:15"),
-        ExpenseDomain("McDonald", price = 77.82, pic = "img2", time = "16 June 2025 13:57"),
-        ExpenseDomain("Cinema", price = 23.47, pic = "img3", time = "16 June 2025 20:45"),
-        ExpenseDomain("Restaurant", price = 573.12, pic = "img4", time = "15 June 2025 22:18")
+        ExpenseDomain("Restaurant", price = 573.12, pic = "restaurant", time = "17 June 2025 19:15"),
+        ExpenseDomain("McDonald", price = 77.82, pic = "mcdonald", time = "16 June 2025 13:57"),
+        ExpenseDomain("Cinema", price = 23.47, pic = "cinema", time = "16 June 2025 20:45"),
+        ExpenseDomain("Restaurant", price = 573.12, pic = "restaurant", time = "15 June 2025 22:18")
     )
     MainScreen(expenses = expenses)
 }
@@ -46,6 +48,7 @@ fun MainScreen(
             item { HeaderSection() }
             item { CardSection(onClick = onCardClick) }
             item { ActionButtonRow() }
+            items(expenses) { item -> ExpenseItem(item) }
         }
     }
 }
--- app/src/main/java/com/example/myfinanceapp/Repository/MainRepository.kt
+++ app/src/main/java/com/example/myfinanceapp/Repository/MainRepository.kt
@@ -4,9 +4,9 @@ import com.example.myfinanceapp.Domain.ExpenseDomain
 
 class MainRepository {
     val items = mutableListOf(
-        ExpenseDomain("Restaurant", price = 573.12, pic = "img1", time = "17 June 2025 19:15"),
-        ExpenseDomain("McDonald", price = 77.82, pic = "img2", time = "16 June 2025 13:57"),
-        ExpenseDomain("Cinema", price = 23.47, pic = "img3", time = "16 June 2025 20:45"),
-        ExpenseDomain("Restaurant", price = 573.12, pic = "img4", time = "15 June 2025 22:18")
+        ExpenseDomain("Restaurant", price = 573.12, pic = "restaurant", time = "17 June 2025 19:15"),
+        ExpenseDomain("McDonald", price = 77.82, pic = "mcdonald", time = "16 June 2025 13:57"),
+        ExpenseDomain("Cinema", price = 23.47, pic = "cinema", time = "16 June 2025 20:45"),
+        ExpenseDomain("Restaurant", price = 573.12, pic = "restaurant", time = "15 June 2025 22:18")
     )
 }
