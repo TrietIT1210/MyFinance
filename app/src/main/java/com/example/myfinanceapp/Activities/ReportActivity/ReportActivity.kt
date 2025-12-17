dc084a2512b8de23a519f6ff596e2cab78502dbc
TrietIT1210 <trietminh1210@gmail.com>
2025-12-17 12:42:32 +0700

    Add report budget screen

---
+++ app/src/main/java/com/example/myfinanceapp/Activities/ReportActivity/ReportActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myfinanceapp.Activities.ReportActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myfinanceapp.R
+
+class ReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/myfinanceapp/Repository/MainRepository.kt
+++ app/src/main/java/com/example/myfinanceapp/Repository/MainRepository.kt
@@ -1,5 +1,6 @@
 package com.example.myfinanceapp.Repository
 
+import com.example.myfinanceapp.Domain.BudgetDomain
 import com.example.myfinanceapp.Domain.ExpenseDomain
 
 class MainRepository {
@@ -9,4 +10,11 @@ class MainRepository {
         ExpenseDomain("Cinema", price = 23.47, pic = "cinema", time = "16 June 2025 20:45"),
         ExpenseDomain("Restaurant", price = 573.12, pic = "restaurant", time = "15 June 2025 22:18")
     )
+
+    val budget = mutableListOf(
+        BudgetDomain("Home Load",1200.0,80.8),
+        BudgetDomain("Subscription",550.0,10.0),
+        BudgetDomain("Car Loan",800.0,30.0),
+    )
+
 }
