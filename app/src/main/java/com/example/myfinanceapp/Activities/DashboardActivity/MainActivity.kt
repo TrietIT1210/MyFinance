d4cdb6ae670749c92113bec4e325f3ccc465b68b
TrietIT1210 <trietminh1210@gmail.com>
2025-12-17 11:35:05 +0700

    Add navigationBar with some features

--- app/src/main/java/com/example/myfinanceapp/Activities/DashboardActivity/MainActivity.kt
+++ app/src/main/java/com/example/myfinanceapp/Activities/DashboardActivity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,15 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myfinanceapp.Activities.DashboardActivity.screens.MainScreen
+import com.example.myfinanceapp.ViewModel.MainViewModel
 import com.example.myfinanceapp.ui.theme.MyFinanceAppTheme
 
 class MainActivity : ComponentActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyFinanceAppTheme {
+                MainScreen(expenses = mainViewModel.loadData(), onCardClick = {
 
+                })
             }
         }
     }
