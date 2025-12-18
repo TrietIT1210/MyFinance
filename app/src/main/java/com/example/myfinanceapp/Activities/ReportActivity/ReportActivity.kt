b1ca68409b0dc8633728449a25ca06f08cdb7f19
TrietIT1210 <trietminh1210@gmail.com>
2025-12-18 18:08:42 +0700

    Complete report screen

--- app/src/main/java/com/example/myfinanceapp/Activities/ReportActivity/ReportActivity.kt
+++ app/src/main/java/com/example/myfinanceapp/Activities/ReportActivity/ReportActivity.kt
@@ -1,14 +1,26 @@
 package com.example.myfinanceapp.Activities.ReportActivity
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import com.example.myfinanceapp.Activities.ReportActivity.screens.ReportScreen
 import com.example.myfinanceapp.R
+import com.example.myfinanceapp.ViewModel.MainViewModel
 
 class ReportActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            ReportScreen(
+                budgets = mainViewModel.loadBudget(),
+                onBack = { finish() }
+            )
+        }
     }
 }
--- app/src/main/java/com/example/myfinanceapp/Activities/ReportActivity/screens/ReportScreen.kt
+++ app/src/main/java/com/example/myfinanceapp/Activities/ReportActivity/screens/ReportScreen.kt
@@ -1,20 +1,28 @@
 package com.example.myfinanceapp.Activities.ReportActivity.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.myfinanceapp.Activities.DashboardActivity.components.BottomNavigationBar
+import com.example.myfinanceapp.Activities.ReportActivity.components.BudgetItem
 import com.example.myfinanceapp.Activities.ReportActivity.components.CenterStatsCard
 import com.example.myfinanceapp.Activities.ReportActivity.components.GradientHeader
 import com.example.myfinanceapp.Activities.ReportActivity.components.SummaryColumns
@@ -27,7 +35,7 @@ fun ReportScreen(
     onBack: () -> Unit
 ) {
     ConstraintLayout (modifier = Modifier.fillMaxSize()) {
-        val (scrollRef, bottomBarRef) = createRefs()
+        val (scrollRef, bottomNavRef) = createRefs()
 
         ReportContent(
             budgets = budgets,
@@ -36,9 +44,24 @@ fun ReportScreen(
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    //bottom.linkTo(parent.bottom)
+                    bottom.linkTo(bottomNavRef.top)
                 },
             onBack = onBack
+        )
+
+        BottomNavigationBar(
+            modifier = Modifier
+                .height(80.dp)
+                .constrainAs (bottomNavRef) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            onItemSelected = { itemId ->
+                if (itemId == R.id.wallet) {
+
+                }
+            }
         )
     }
 }
@@ -93,6 +116,29 @@ fun ReportContent(
                 )
                 .padding(8.dp)
         ) }
+        item {
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+                    .padding(horizontal = 24.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    "My budget",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 19.sp,
+                    color = colorResource(R.color.darkBlue)
+                )
+                Text(
+                    "Edit",
+                    color = colorResource(R.color.darkBlue)
+                )
+            }
+        }
+        itemsIndexed (budgets) { index, item ->
+            BudgetItem(budget = item, index = index)
+        }
     }
 }
 
