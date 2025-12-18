3e0d103eaf29ab8b88cdce793aac02bf24b489a5
TrietIT1210 <trietminh1210@gmail.com>
2025-12-18 13:35:09 +0700

    Add report screen

--- app/src/main/java/com/example/myfinanceapp/Activities/ReportActivity/screens/ReportScreen.kt
+++ app/src/main/java/com/example/myfinanceapp/Activities/ReportActivity/screens/ReportScreen.kt
@@ -4,14 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.myfinanceapp.Activities.ReportActivity.components.CenterStatsCard
 import com.example.myfinanceapp.Activities.ReportActivity.components.GradientHeader
 import com.example.myfinanceapp.Domain.BudgetDomain
 
@@ -30,7 +32,7 @@ fun ReportScreen(
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    bottom.linkTo(bottomBarRef.bottom)
+                    //bottom.linkTo(parent.bottom)
                 },
             onBack = onBack
         )
@@ -51,7 +53,7 @@ fun ReportContent(
             ConstraintLayout(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(250.dp)
+                .height(420.dp)
             ) {
                 val (header, card) = createRefs()
                 GradientHeader (
@@ -64,6 +66,18 @@ fun ReportContent(
                             end.linkTo(parent.end)
                         },
                     onBack = onBack
+                )
+                CenterStatsCard(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(horizontal = 24.dp)
+                        .constrainAs(card) {
+                            top.linkTo(header.bottom)
+                            bottom.linkTo(header.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
                 )
             }
         }
